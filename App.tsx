84ac571d67272f45f5205fed2e227e27d0836b2c
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2024-08-07 19:04:21 +0530

    UI Design Completed till 07/08/2024

--- App.tsx
+++ App.tsx
@@ -1,118 +1,253 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+// import AppStyle from './assets/AppStyle';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <SafeAreaView style={AppStyle.mainContainer1}>
+        <View style={AppStyle.topView1ofMainContainer1}>
+          <Text style={AppStyle.topView1ofMainContainer1Text}>Service at your doorstep.</Text>
+
+          <Image source={require('./assets/Image/notificationbing.png')} style={AppStyle.topView1ofMainContainer1Image} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        <View style={AppStyle.topView2ofMainContainer1}>
+          <TextInput placeholder='Search' placeholderTextColor={'#B3B3B3'} style={AppStyle.topView2ofMainContainer1Text} />
+
+          <View style={AppStyle.topView2ofMainContainer1ImageView}>
+            <Image source={require('./assets/Image/searchnormal.png')} style={AppStyle.topView2ofMainContainer1Image} />
+          </View>
+          
+        </View>
+
+        <View style={{width:'100%', height:227,alignItems:'center',}}>
+          <Image source={require('./assets/Image/banner4x.png')} style={AppStyle.topView3ofMainContainer1Image1} />
+
+          <View style={{flexDirection:'row'}}>
+            <Image source={require('./assets/Image/Rectangle1.png')} style={AppStyle.topView3ofMainContainer1Image2} />
+
+            <Image source={require('./assets/Image/Rectangle.png')} style={AppStyle.topView3ofMainContainer1Image3} />
+
+            <Image source={require('./assets/Image/Rectangle.png')} style={AppStyle.topView3ofMainContainer1Image3} />
+          </View>
+          
+        </View>
+
+        <View>
+          <Text>hello</Text>
+        </View>
+
+        <TouchableOpacity style={AppStyle.topView5ofMainContainer1}>
+
+          <View style={AppStyle.topView5ofMainContainer1View}>
+
+            <Text style={AppStyle.topView5ofMainContainer1ViewText}>Go to Admin Dashboard</Text>
+            <Image source={require('./assets/Image/arrowright.png')} style={AppStyle.topView5ofMainContainer1ViewImage} />
+
+          </View>
+          
+        </TouchableOpacity>
+
+      </SafeAreaView>
+
+      <SafeAreaView style={AppStyle.mainContainer2}>
+
+        <View style={AppStyle.bottomFixViewContainer}>
+
+          <View >
+            <Image source={require('./assets/Image/chart.png')} style={AppStyle.bottomFixViewImageContain} />
+          </View>
+
+          <View style={AppStyle.bottomFixDashBordContainer}>
+            <Image source={require('./assets/Image/element4.png')} style={AppStyle.bottomFixViewImageContain} />
+
+            <Text style={AppStyle.bottomFixDashBordText}>Dashboard</Text>
+          </View>
+
+          <View>
+            <Image source={require('./assets/Image/crown.png')} style={AppStyle.bottomFixViewImageContain} />
+          </View>
+
+          <View>
+            <Image source={require('./assets/Image/profile.png')} style={AppStyle.bottomFixViewImageContain} />
+          </View>
+
+
+        </View>
+      </SafeAreaView>
+    </>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const AppStyle = StyleSheet.create({
+  mainContainer1:{
+    backgroundColor:'gray',
+    height:'94%', 
+    // top:47,
+    paddingHorizontal:18,
+    justifyContent:'space-evenly',
+    // alignItems:'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  mainContainer2:{
+      flex:1,
+      justifyContent:'flex-end'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  bottomFixViewContainer:{
+      flexDirection:'row',
+      height:56,
+      width:'100%',
+      backgroundColor:'#585858',
+      justifyContent:'space-evenly',
+      alignItems:'center',
   },
-  highlight: {
-    fontWeight: '700',
+
+  bottomFixViewImageContain:{
+    width:24,
+    height:24,
   },
+
+  bottomFixDashBordContainer:{
+    backgroundColor:'#F15E64',
+    paddingHorizontal:12,
+    paddingVertical:6, 
+    borderRadius:8,
+    width:123, 
+    height:36, 
+    flexDirection:'row', justifyContent:'space-around', alignItems:'center',
+  },
+
+  bottomFixDashBordText:{
+    fontFamily:'OswaldRegular', 
+    fontSize:12, 
+    fontWeight:'500',
+    color:'#FFFFFF'
+  },
+
+  topView1ofMainContainer1:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'flex-start',
+    width:'100%',
+    height:78,
+  },
+
+  topView1ofMainContainer1Text:{
+    fontFamily:'OswaldRegular', 
+    fontSize:24, 
+    fontWeight:'500',
+    width:'50%',
+    color:'#FFFFFF'
+  },
+
+  topView1ofMainContainer1Image:{
+    width:24,
+    height:24, 
+    marginTop:12,
+  },
+
+  topView2ofMainContainer1:{
+    width:'100%',
+    height:68,
+    borderRadius:12,
+    backgroundColor:'#585858',
+    flexDirection:'row',
+    paddingHorizontal:10,
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+
+  topView2ofMainContainer1Text:{
+    fontFamily:'OswaldRegular', 
+    fontSize:18, 
+    fontWeight:'400',
+    width:'88%'
+  },
+
+  topView2ofMainContainer1ImageView:{
+    height:40, 
+    width:38, 
+    backgroundColor:'#F15E64', justifyContent:'center', 
+    alignItems:'center', 
+    borderRadius:8, 
+  },
+
+  topView2ofMainContainer1Image:{
+    width:24,
+    height:24,
+  },
+  
+  topView3ofMainContainer1Image1:{
+    height:200,
+    width:'100%', 
+    resizeMode:'stretch'
+  },
+
+  topView3ofMainContainer1Image2:{
+    width:8, 
+    height:16, 
+    marginHorizontal:2, 
+    marginTop:5
+  },
+
+  topView3ofMainContainer1Image3:{
+    width:8, 
+    height:8, 
+    marginHorizontal:2, 
+    marginTop:5, 
+    alignSelf:'center'
+  },
+
+
+
+
+
+  topView5ofMainContainer1:{
+    paddingHorizontal:25, 
+    paddingVertical:17, 
+    backgroundColor:'#F15E64', 
+    borderRadius:12
+  },
+
+  topView5ofMainContainer1View:{
+    flexDirection:'row', justifyContent:'space-between', alignItems:'center'
+  },
+
+  topView5ofMainContainer1ViewText:{
+    fontFamily:'OswaldRegular', 
+    fontSize:16, 
+    color:'#FFFFFF',
+    fontWeight:'500'
+  },
+
+  topView5ofMainContainer1ViewImage:{
+    width:24,
+    height:24
+  },
+
+
+
+
+
+
 });
 
 export default App;
---
+++ assets/AppStyle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+export default AppStyle = StyleSheet.create({
+
+    mainContainer:{
+        flex:1,
+        backgroundColor:'wheat',
+        // color:
+    },
+
+    bottomFixView:{
+        position:'fixed',
+        top:732,
+        flexDirection:'row',
+        height:56,
+        width:'100%',
+        backgroundColor:'#585858',
+        justifyContent:'space-evenly',
+        alineItem:''
+    },
+  
+});
